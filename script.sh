00e9e0ca7d00167f3d76924d98626963064fc9e8
dennisangemi <dennisangemi@gmail.com>
2024-05-24 18:23:56 +0200

    first commit

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# contsants
+IDS_FILE="cresime.txt"
+CANTI_DIR="canti"
+
+# load function from file
+source get_text_from_id
+
+# crea directory CANTI_DIR se non esiste
+mkdir -p $CANTI_DIR
+
+# per ogni riga del file IDS_FILE, poni id uguale alla riga corrente
+while IFS= read -r id
+do
+    # scarica il testo del canto con id uguale alla riga corrente
+    get_text_from_id $id > $CANTI_DIR/$id.txt
+done < "$IDS_FILE"
+
+
